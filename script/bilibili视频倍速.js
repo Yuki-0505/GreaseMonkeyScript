6226778455207d34c4285ff840293f589476ed35
Yuki-0505 <2243104010@qq.com>
2021-02-15 22:21:24 +0800

    视频倍速

--- script/bilibili视频倍速.js
+++ script/bilibili视频倍速.js
@@ -11,23 +11,35 @@
 // ==/UserScript==
 (function () {
   'use strict'
-  let video = document.querySelector('video')
+  const video = document.querySelector('video')
   let interval = setInterval(function () {
     let ul = document.querySelector('.bilibili-player-video-btn-speed-menu')
-    if (!ul) return
+    let li = document.querySelector('.bilibili-player-video-btn-speed-menu-list')
+    if (!ul || !li) return
     clearInterval(interval)
-    for (let speed of ['1.75', '2.0', '2.5', '3.0', '4.0']) {
-      let li = document.createElement('li')
-      li.setAttribute('class', 'bilibili-player-video-btn-speed-menu-list')
-      li.setAttribute('data-value', Number(speed))
-      li.innerHTML = speed + 'x'
-      li.addEventListener('click', function () {
-        let lis = [...document.querySelectorAll('.bilibili-player-video-btn-speed-menu-list')]
-        lis.forEach(item => item.setAttribute('class', 'bilibili-player-video-btn-speed-menu-list'))
-        this.setAttribute('class', 'bilibili-player-video-btn-speed-menu-list bilibili-player-active')
-        video.playbackRate = this.getAttribute('data-value')
-      })
-      ul.appendChild(li)
+    ul.innerHTML = ''
+    let lis = []
+    for (let speed of ['4.0', '3.0', '2.5', '2.0', '1.75', '1.5', '1.25', '1.0', '0.75', '0.5']) {
+      // let li = document.createElement('li')
+      // let classValue = 'bilibili-player-video-btn-speed-menu-list'
+      // classValue += speed === '1.0'? ' bilibili-player-active': ''
+      // li.setAttribute('class', classValue)
+      // li.setAttribute('data-value', speed)
+      // li.innerHTML = speed + 'x'
+      // li.addEventListener('click', function () {
+      //   let select_li = document.querySelector('.bilibili-player-video-btn-speed-menu-list.bilibili-player-active')
+      //   select_li.setAttribute('class', 'bilibili-player-video-btn-speed-menu-list')
+      //   this.setAttribute('class', 'bilibili-player-video-btn-speed-menu-list bilibili-player-active')
+      //   video.playbackRate = Number(this.getAttribute('data-value'))
+      //   let btnValue = this.getAttribute('data-value') === '1.0'? '倍速': this.getAttribute('data-value') + 'x'
+      //   document.querySelector('.bilibili-player-video-btn-speed-name').innerHTML = btnValue
+      // })
+      // ul.appendChild(li)
+      let li2 = li.cloneNode()
+      li2.setAttribute('data-value', speed)
+      li2.innerHTML = speed + 'x'
+      ul.appendChild(li2)
     }
   }, 100)
 })()
+// <li class="bilibili-player-video-btn-speed-menu-list bilibili-player-active" data-value="1">1.0x</li>
