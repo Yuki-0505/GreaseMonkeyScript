afc7597f9c091f35894a2eb52895ed3bf90dbda5
Yuki-0505 <2243104010@qq.com>
2021-02-16 22:27:22 +0800

    脚本代码模板

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
-// ==UserScript==
-// @name          ${1:name}
-// @namespace     ${2:namespace}
-// @version       ${3:0.1}
-// @description   ${4:description}
-// @author        ${5:Pink-0505}
-// @match         ${6|http,https|}://$2/*
-// @grant         ${8|none,GM_setValue,GM_getValue,GM_download,GM_xmlhttpRequest|}
-// @connect       ${9:example.com}
-// @require       ${10:https://code.jquery.com/jquery-3.5.1.min.js}
-// @license       ${11|AGPL-3.0|}
-// @icon          ${6|http,https|}://$2/favicon.ico
-// ==/UserScript==
-;(function () {
-  'use strict'
-  $12
-})()
+const fs = require('fs')
+const path = require('path')
+
+fs.readFile(path.join(__dirname, './template.js'), 'utf8', (err, data) => {
+  if (err) return console.log('error reading.')
+  fs.writeFile(path.join(__dirname, './result.js'), replace(data), err => {
+    if (err) return console.log('error writing.')
+  })
+})
+
+function replace(data) {
+  let result = data.replace(/\n+/g, `\n`).replace(/\n\s{2}/g, `\n\\t`)
+  while (/\n(\\t)+\s{2}/g.test(result)) {
+    result = result.replace(/\\t\s{2}/g, `\\t\\t`)
+  }
+  result = `"` + result.replace(/\n/g, `",\n"`).replace(/,\n"$/g, ``)
+  return result
+}
